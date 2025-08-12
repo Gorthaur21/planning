f191c30db04bbf446a55e48bd87c1b1b09c70872
Gorthaur21 <davidreuter59@gmail.com>
2025-08-12 11:37:04 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "1:XXXXXXX:web:XXXXXXXX"
-};
+    apiKey: "AIzaSyAp9f2Cbu0iUojvRH1FdDrGEONg1TQ52wg",
+    authDomain: "planning-equipe-573e0.firebaseapp.com",
+    projectId: "planning-equipe-573e0",
+    storageBucket: "planning-equipe-573e0.firebasestorage.app",
+    messagingSenderId: "519775278154",
+    appId: "1:519775278154:web:247f93ea37279a7ec782b3"
+  };
 
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
